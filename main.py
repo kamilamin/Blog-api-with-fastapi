2824023cd390a8f7fbaea14bb7e84b225bbe4a39
kamilamin <kamil.amin@live.co.uk>
2021-03-28 13:17:09 +0500

    --Commit: Developed post and get blog apis, Setup DB and connected

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # @app is path operation decortor / Base path and get function
-@app.get('/') 
-def index(): #Path operation function
-    return {'data': 'Blog list'}
+@app.get('/blogs') 
+def index(limit: int, published: bool): #Path operation function
+    if published:
+        return {'data': f"{limit} published blogs from the db."}
+    else:
+        return {'data': f"{limit} blogs from the db."}
 
 # About path and get function 
 @app.get('/blogs/{id}') #Path operation function
@@ -16,3 +21,11 @@ def fetchBlogsById(id):
 def getBlogComments(id):
     return {"data": {"1","2"}}
 
+class blogModel(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/createBlogs')
+def create_blog(request: blogModel):
+    return {'data': f"Blog is create with title as {request.title}"}
